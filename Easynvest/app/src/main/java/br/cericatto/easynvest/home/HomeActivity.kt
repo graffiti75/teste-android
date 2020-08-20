d24a008546f2ef6ca172ba964da6e77fce77de73
Rodrigo Cericatto <graffiti75@gmail.com>
2020-08-20 15:48:58 -0300

    Pacotes home e result criados.

--- Easynvest/app/src/main/java/br/cericatto/easynvest/home/HomeActivity.kt
+++ Easynvest/app/src/main/java/br/cericatto/easynvest/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package br.cericatto.easynvest
+package br.cericatto.easynvest.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.cericatto.easynvest.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
